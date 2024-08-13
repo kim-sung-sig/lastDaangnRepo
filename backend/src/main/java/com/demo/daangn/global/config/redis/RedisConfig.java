a2b45d5b273bb4436b14c3bd5ab209f1e2b1f293
kimc <tjdtlr12349@gmail.com>
2024-08-13 23:55:02 +0900

    몰루ㅠㅜ

---
+++ backend/src/main/java/com/demo/daangn/global/config/redis/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.demo.daangn.global.config.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // key와 value의 직렬화 방식을 StringRedisSerializer로 지정
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+}
--- backend/src/main/java/com/demo/daangn/global/config/websocket/WebSocketConfig.java
+++ backend/src/main/java/com/demo/daangn/global/config/websocket/WebSocketConfig.java
@@ -1,17 +1,10 @@
 package com.demo.daangn.global.config.websocket;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
-import org.springframework.messaging.handler.invocation.HandlerMethodReturnValueHandler;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -33,40 +26,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
                 .withSockJS()
                 // .setInterceptors(new CustomWebSocketInterceptors())
                 ;
-    }
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        WebSocketMessageBrokerConfigurer.super.addArgumentResolvers(argumentResolvers);
-    }
-
-    @Override
-    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
-        WebSocketMessageBrokerConfigurer.super.addReturnValueHandlers(returnValueHandlers);
-    }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        WebSocketMessageBrokerConfigurer.super.configureClientInboundChannel(registration);
-    }
-
-    @Override
-    public void configureClientOutboundChannel(ChannelRegistration registration) {
-        WebSocketMessageBrokerConfigurer.super.configureClientOutboundChannel(registration);
-    }
-
-    @Override
-    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-        return WebSocketMessageBrokerConfigurer.super.configureMessageConverters(messageConverters);
-    }
-
-    @Override
-    public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
-        WebSocketMessageBrokerConfigurer.super.configureWebSocketTransport(registry);
-    }
-
-    @Override
-    public Integer getPhase() {
-        return WebSocketMessageBrokerConfigurer.super.getPhase();
     }
 
 }
