c3c6a1902e1f14c4127117c6ac26acf8a75baf99
kimc <tjdtlr12349@gmail.com>
2024-08-14 08:04:16 +0900

    ㅁㄴㅁㄴㄹ

--- backend/src/main/java/com/demo/daangn/global/config/redis/RedisConfig.java
+++ backend/src/main/java/com/demo/daangn/global/config/redis/RedisConfig.java
@@ -1,16 +1,24 @@
 package com.demo.daangn.global.config.redis;
 
+import java.time.Duration;
+import java.util.Arrays;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
 
@@ -18,6 +26,31 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
 
+        // Hash Key-Value 형태로 직렬화를 수행합니다.
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
         return redisTemplate;
+    }
+
+    @Bean
+    CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(1))  // 캐시의 기본 TTL 설정
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+
+    @Bean
+    //  이거는 수정해야댐
+    KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
+            // 원하는 키 생성 로직 구현
+            return method.getName() + "_" + Arrays.toString(params);
+        };
     }
 }
