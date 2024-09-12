afcc35e5f22a156845ff2d10008152936bbcc9a9
kim-sung-sig <144510755+kim-sung-sig@users.noreply.github.com>
2024-09-12 13:23:57 +0900

    Update RedisUtils.java

--- backend/src/main/java/com/demo/daangn/global/util/redis/RedisUtils.java
+++ backend/src/main/java/com/demo/daangn/global/util/redis/RedisUtils.java
@@ -1,7 +1,7 @@
 package com.demo.daangn.global.util.redis;
 
 import java.util.concurrent.TimeUnit;
-
+import java.util.Optional;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +14,7 @@ public class RedisUtils {
     private final RedisTemplate<String, Object> redisTemplate;
 
     /**
-     * Redis에 데이터를 저장합니다.
+     * Redis에 데이터를 저장합니다. (만료 시간이 있는 경우)
      *
      * @param prefix Redis 키의 프리픽스 (예: "post", "user" 등)
      * @param key Redis에 저장할 키 (프리픽스를 포함하지 않은 값)
@@ -25,6 +25,19 @@ public class RedisUtils {
         String redisKey = buildRedisKey(prefix, key);
         redisTemplate.opsForValue().set(redisKey, value, expirationTime, TimeUnit.SECONDS);
     }
+    
+    /**
+     * Redis에 데이터를 저장합니다. (만료 시간이 없는 경우)
+     *
+     * @param prefix Redis 키의 프리픽스 (예: "post", "user" 등)
+     * @param key Redis에 저장할 키 (프리픽스를 포함하지 않은 값)
+     * @param value Redis에 저장할 값
+     * @param expirationTime 데이터의 만료 시간 (초 단위)
+     */
+    public void set(String prefix, String key, Object value) {
+        String redisKey = buildRedisKey(prefix, key);
+        redisTemplate.opsForValue().set(redisKey, value);
+    }
 
     /**
      * Redis에서 데이터를 조회합니다.
@@ -33,14 +46,14 @@ public class RedisUtils {
      * @param key Redis에서 조회할 키 (프리픽스를 포함하지 않은 값)
      * @return Redis에 저장된 값, 없으면 null
      */
-    public <T> T get(String prefix, String key, Class<T> type) {
+    public <T> Optional<T> get(String prefix, String key, Class<T> type) {
         String redisKey = buildRedisKey(prefix, key);
         Object value = redisTemplate.opsForValue().get(redisKey);
 
         if(value != null && type.isInstance(value)) {
-            return type.cast(value);
+            return Optional.of(type.cast(value));
         }
-        return null;
+        return Optional.empty();
     }
 
     /**
